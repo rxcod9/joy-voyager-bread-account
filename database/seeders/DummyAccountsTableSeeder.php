163d90773e27b233632f27c7f7f17848507c515e
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyAccountsTableSeeder.php
+++ database/seeders/DummyAccountsTableSeeder.php
@@ -14,7 +14,7 @@ class DummyAccountsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Account::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
